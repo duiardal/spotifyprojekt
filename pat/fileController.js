ecb1648e2193b0856522618bad749ca175267e7b
Patrik Odqvist <patrikodqvist@gmail.com>
2017-04-18 20:46:35 +0200

    Delete fileController.js

--- pat/fileController.js
+++
@@ -1,9 +0,0 @@
-kolCalc.controller('FileUpload', function ($scope,calc) {
-	$scope.installCategories = function() {
-		calc.getCategory.get({}, function(output){
-			calc.createCategories(output.categories);
-			calc.cat=output.categories;
-		});
-
-	}
-});
